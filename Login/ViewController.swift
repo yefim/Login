d1e290eae324feddda6c857333521be001f21a35
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-12-03 21:42:52 -0800

    Add login view

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    @IBAction func onNextPress(_ sender:UITextField) {
+        // Focus password field
+        emailField.resignFirstResponder()
+        passwordField.becomeFirstResponder()
+    }
+
+    @IBAction func onGoPress(_ sender:UITextField) {
+        handleLogin()
+    }
+
+    @IBAction func onLoginClick(_ sender:UIButton) {
+        handleLogin()
+    }
+
+    func handleLogin() {
+        let email = emailField.text
+        let password = passwordField.text
+
+        print("Email \(email) Password \(password)")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
